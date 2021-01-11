279e9dda18e0fc495853e0fba183ffe3a44e8d0b
ApothecaryOfLight <ChangerOfWays@gmail.com>
2021-01-11 17:12:25 +0000

    Refined schema creation script.

--- create_schema.sh
+++ create_schema.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm test.txt
+rm create_schema.sql
 
 #Make month tables for temporal searches
 YEAR=2015
@@ -7,7 +7,7 @@ MONTH=1
 while [ $YEAR -lt 2022 ]; do
   let MONTH=1
   while [ $MONTH -lt 13 ]; do
-    echo "CREATE TABLE " calendar_$YEAR-$MONTH " ( day INT, article_id FOREIGN KEY REFERENCES articles.article_id ) " >> "test.txt"
+    echo "CREATE TABLE " calendar_$YEAR_$MONTH " ( day INT, article_id_fk INT, article_title VARCHAR(150), FOREIGN KEY article_id_fk REFERENCES articles(article_id) ON DELETE CASCADE ); " >> "create_schema.sql"
     let MONTH=MONTH+1
   done
   let YEAR=YEAR+1
@@ -17,8 +17,8 @@ done
 alphabet=( A B C D E F G H I J K L M N O P Q R S T U V W X Y Z )
 for letter in ${alphabet[@]}; do
   for sub_letter in ${alphabet[@]}; do
-    echo "CREATE TABLE" $letter"-"$sub_letter "( word CHAR(50) PRIMARY KEY, article INT FOREIGN KEY REFERENCES articles.article_id )" >> "test.txt"
+    echo "CREATE TABLE" $letter"_"$sub_letter "( word CHAR(50) PRIMARY KEY, article_id_fk INT, article_title VARCHAR(150), FOREIGN KEY article_id_fk REFERENCES (articles)article_id ON DELETE CASCADE );" >> "create_schema.sql"
   done
 done
 
-echo "CREATE TABLE master_article_list ( VARCHAR(150) article_title, INT article_id PRIMARY KEY, article_text BLOB )" >> "test.txt"
+echo "CREATE TABLE articles( article_title VARCHAR(150), article_id INT PRIMARY KEY, article_text BLOB  );" >> "create_schema.sql"
--- setup.sh
+++ setup.sh
@@ -1,19 +1,24 @@
+#!/bin/bash
 sudo apt-get upgrade -y sudo apt-get update -y
 
+
+#==NODEJS==
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 
+#==MYSQL==
 sudo apt install mysql-server -y
 # TODO: Get this script to run without user input
+sudo mysql_secure_installation
+./create_schema.sh
+sudo mysql < create_schema.sh
 
+
+#==NGINX==
 sudo apt-get install nginx -y
 sudo ufw allow 'Nginx HTTP'
 sudo ufw allow ssh
 sudo ufw enable
-# TODO: Configure etc/nginx/sites-enabled to point to correct directory
-sudo mysql_secure_installation
-
 cd etc/nginx/sites-enabled && sudo sed -i "s/root \/var\/www\/html;/root \/home\/ubuntu\/DarkYears\/frontend;/g" default
-
 sudo systemctl restart nginx
