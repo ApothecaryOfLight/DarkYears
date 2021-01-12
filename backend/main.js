f5f7eae6d68f105492b0da8dfeb84e7771b0a060
ApothecaryOfLight <ChangerOfWays@gmail.com>
2021-01-12 19:23:08 +0000

    Added SQL function to manage primary key generation, retirement and reissuing.

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,72 @@
-console.log( "Not coded yet." );
+const mysql = require('mysql2');
+
+let mysql_pool;
+function init_mysql_pool() {
+  console.log( "Initializing MySQL pool." );
+  mysql_pool = mysql.createPool({
+    connectionLimit: 50,
+    host: 'localhost',
+    user: 'DarkYears_User',
+    password: 'DarkYears_Password',
+    database: 'DarkYearsDB'
+  });
+
+  do_test();
+}
+
+
+
+function do_test() {
+  mysql_pool.query(
+    'INSERT INTO articles (article_title, article_id, article_text) VALUES (\"testing title\", 1, \"this is the body\" );',
+    function( error, results, fields ) {
+      if( error ) { console.error( error ); return; }
+      console.dir( results );
+      console.dir( fields );
+    }
+  );
+}
+
+
+//init_mysql_pool();
+
+
+let article_title = "testing apart article";
+let article_date = "2015_06_20";
+let article_text = "this is the text of an article for Dark Years.";
+
+function do_process_article_text( inArticleText ) {
+//1) Break apart words, remove 1 and 2 letter words.
+  let punctuation_regex = new RegExp('[^\\w\\s]|[_]','g');
+  let punctuation_removed = inArticleText.replace( punctuation_regex, "" );
+  //console.log( punctuation_removed );
+  let words = punctuation_removed.split(" ");
+  //console.dir( words );
+  let words_obj = {};
+  words.forEach( word => {
+    if( word.length > 2 ) {
+      words_obj[word.toUpperCase()] = {
+        first_letter : word.charAt(0).toUpperCase(),
+        second_letter : word.charAt(1).toUpperCase()
+      }
+    }
+  });
+  console.dir( words_obj );
+//2) Determine first two letters
+}
+
+function do_process_article( inArticleTitle, inArticleDate, inArticleText ) {
+//1) Get article_id (ensure it is reserved, if func fails retire ID).
+  
+//2) Add article_id, title and text to articles table.
+
+//3) Process text body into object.
+  let WordsArray = do_process_article_text( inArticleText );
+//4) Process date into calendar table
+  
+//5) Add words to calendar table.
+
+//6) Add article id to words table
+}
+
+do_process_article( "article_title", "12345", "testing, test. this!_ ye-p!" );
--- create_schema.sh
+++ create_schema.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+\#!/bin/bash
 rm create_schema.sql
+
+echo "DROP DATABASE IF EXISTS DarkYearsDB;" >> "create_schema.sql"
+echo "CREATE DATABASE IF NOT EXISTS DarkYearsDB;" >> "create_schema.sql"
+echo "USE DarkYearsDB;" >> "create_schema.sql"
+
+echo "CREATE TABLE articles( article_title VARCHAR(150), article_id INT PRIMARY KEY, article_text LONGTEXT );" >> "create_schema.sql"
 
 #Make month tables for temporal searches
 YEAR=2015
@@ -7,7 +13,7 @@ MONTH=1
 while [ $YEAR -lt 2022 ]; do
   let MONTH=1
   while [ $MONTH -lt 13 ]; do
-    echo "CREATE TABLE " calendar_$YEAR_$MONTH " ( day INT, article_id_fk INT, article_title VARCHAR(150), FOREIGN KEY article_id_fk REFERENCES articles(article_id) ON DELETE CASCADE ); " >> "create_schema.sql"
+    echo "CREATE TABLE calendar_"$YEAR"_"$MONTH " ( day INT, article_id_fk INT, article_title VARCHAR(150), FOREIGN KEY (article_id_fk) REFERENCES articles(article_id) ON DELETE CASCADE ); " >> "create_schema.sql"
     let MONTH=MONTH+1
   done
   let YEAR=YEAR+1
@@ -17,8 +23,28 @@ done
 alphabet=( A B C D E F G H I J K L M N O P Q R S T U V W X Y Z )
 for letter in ${alphabet[@]}; do
   for sub_letter in ${alphabet[@]}; do
-    echo "CREATE TABLE" $letter"_"$sub_letter "( word CHAR(50) PRIMARY KEY, article_id_fk INT, article_title VARCHAR(150), FOREIGN KEY article_id_fk REFERENCES (articles)article_id ON DELETE CASCADE );" >> "create_schema.sql"
+    echo "CREATE TABLE" $letter"_"$sub_letter "( word CHAR(50) PRIMARY KEY, article_id_fk INT, article_title VARCHAR(150), FOREIGN KEY (article_id_fk) REFERENCES articles(article_id) ON DELETE CASCADE );" >> "create_schema.sql"
   done
 done
 
-echo "CREATE TABLE articles( article_title VARCHAR(150), article_id INT PRIMARY KEY, article_text BLOB  );" >> "create_schema.sql"
+echo "CREATE USER IF NOT EXISTS 'DarkYears_User'@'localhost' IDENTIFIED BY 'DarkYears_Password';" >> "create_schema.sql"
+echo "GRANT ALL ON DarkYearsDB.* TO 'DarkYears_User'@'localhost'" >> create_schema.sql
+
+echo "DELIMITER $$ \
+CREATE FUNCTION DarkYearsDB.generate_new_id() \
+RETURNS BIGINT \
+NOT DETERMINISTIC \
+CONTAINS SQL \
+READS SQL DATA \
+BEGIN \
+DECLARE RetiredID BIGINT; \
+DECLARE LastID BIGINT; \
+SET @RetiredID = (SELECT retired_id FROM sequence_retired LIMIT 1); \
+SET @LastID = (SELECT last FROM sequence_last LIMIT 1); \
+IF @RetiredID IS NULL THEN UPDATE sequence_last SET last = last + 1; \
+ELSE DELETE FROM sequence_retired WHERE retired_id = @RetiredID; \
+END IF; \
+SET @NewID = COALESCE( @RetiredID, @LastID ); \
+RETURN @NewID; \
+END$$ \
+DELIMITER ;" >> create_schema.sql
